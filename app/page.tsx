cf3d462cf05e4a8e066b0b5cc911c9d82a7d06de
estherlardze <esther.odzao@amalitech.com>
2025-03-05 12:29:40 +0000

    modify file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { supabase } from "@/config";
 import { useState } from "react";
 
 
@@ -7,12 +8,20 @@ import { useState } from "react";
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
 
-const handleSubmit = () => {
-    console.log("Submitted", inputValue)
+
+ const createTodo = async() => {
+   const {data, error} = await supabase.from('TodoList').insert({title: inputValue}) 
+
+   if(error){
+    console.log("Error", error)
+   }
+    console.log("Data", data)
+ }
+
   return (
     <div>
       <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
+      <button type="submit" onClick={createTodo}>Submit</button>
     </div>
-  );
+  )
 }
--- config.ts
+++ config.ts
@@ -1,9 +1,13 @@
 import {createClient} from '@supabase/supabase-js';
 
 
-const superbaseURL = process.env.NEXT_PUBLIC_API_URL
+const superbaseURL=process.env.NEXT_PUBLIC_API_URL 
 
-const supabaseKEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabaseKEY = process.env.NEXT_PUBLIC_API_KEY;
+
+if(!supabaseKEY || !superbaseURL){
+    throw new Error('Missing Supabase credentials')
+}
 
 
-export const supabase = createClient(superbaseURL, supabaseKEY);
+export const supabase = createClient(supabaseKEY, superbaseURL);
